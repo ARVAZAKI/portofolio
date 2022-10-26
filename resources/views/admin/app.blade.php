1e61abc37c583eeedc61c2bd26ff594ce804c9d5
ARVAZAKI <arvazf09@gmail.com>
2022-10-26 07:43:29 +0700

    commit

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -14,9 +14,9 @@
 
     <!-- Custom fonts for this template-->
     <link href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css>
-    <link
+    {{-- <link
         href={{ asset('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') }}
-        rel="stylesheet"  type="text/css">
+        rel="stylesheet"  type="text/css"> --}}
 
     <!-- Custom styles for this template-->
     <link href={{ asset('template/css/sb-admin-2.min.css') }} rel="stylesheet"  type="text/css">
