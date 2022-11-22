4f99d5af5872586e50ac94e984a5a65288468def
ARVAZAKI <arvazf09@gmail.com>
2022-11-22 13:49:32 +0700

    commit

--- resources/views/showcontact.blade.php
+++ resources/views/showcontact.blade.php
@@ -6,14 +6,10 @@
 @foreach($data as $contact)
 <div class="card">
 <div class="card-header">
-    {{ $contact->jenis_kontak}}
+    Kontak siswa
 </div>
 <div class="card-body">
-    @foreach ($data->jenis_kontak as $p)
-            <h6>contact : {{ $p->jenis_kontak }}</h6>
-            @endforeach
     <h6>Deskripsi kontak : {{ $contact->deskripsi}}</h6>
-    
 </div>
 <div class="card-footer">
     <a href="{{ route('mastercontact.edit',$contact->id)}}" class="btn btn-se btn-warning"><i class="fas fa-edit"></i></a>
